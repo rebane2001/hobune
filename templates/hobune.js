9575007bb6ff2157161c23787e210e581d621d57
rebane2001 <rebane2001@gmail.com>
2024-05-12 14:58:34 +0300

    Formatting

--- templates/hobune.js
+++ templates/hobune.js
@@ -1,68 +1,68 @@
 function channelSort() {
-	const sortOption = document.querySelector(".sort").value;
-	const [sortBy, direction] = sortOption.split("-");
-	const isInt = sortBy !== "search";
-	const container = document.querySelector(".channels.flex-grid");
-	[...container.children]
-		.sort((a,b)=>{
-			const dir = direction ? 1 : -1;
-			let valA = a.dataset[sortBy];
-			let valB = b.dataset[sortBy];
-			if (isInt) {
-				valA = parseInt(valA);
-				valB = parseInt(valB);
-			}
-			return (valA>valB?1:-1)*dir;
-		})
-		.forEach(node=>container.appendChild(node));
+  const sortOption = document.querySelector(".sort").value;
+  const [sortBy, direction] = sortOption.split("-");
+  const isInt = sortBy !== "search";
+  const container = document.querySelector(".channels.flex-grid");
+  [...container.children]
+    .sort((a,b)=>{
+      const dir = direction ? 1 : -1;
+      let valA = a.dataset[sortBy];
+      let valB = b.dataset[sortBy];
+      if (isInt) {
+        valA = parseInt(valA);
+        valB = parseInt(valB);
+      }
+      return (valA>valB?1:-1)*dir;
+    })
+    .forEach(node=>container.appendChild(node));
 }
 
 function getSearchRegex(searchTerm) {
-	const regexParts = searchTerm.match(/^\/(.*)\/([dgimsuvy]*)$/);
-	try {
-		return regexParts && new RegExp(regexParts[1], regexParts[2]);
-	} catch(err) { console.error(err); }
+  const regexParts = searchTerm.match(/^\/(.*)\/([dgimsuvy]*)$/);
+  try {
+    return regexParts && new RegExp(regexParts[1], regexParts[2]);
+  } catch(err) { console.error(err); }
 }
 
 function channelSearch() {
-	let searchTerm = document.querySelector(".search").value;
-	
-	const allowedClasses = [];
-	const filteredClasses = [];
-	const availableClasses = ["unlisted", "removed"];
-	for (const availableClass of availableClasses) {
-		if (searchTerm.includes("!" + availableClass)) {
-			filteredClasses.push(availableClass);
-			searchTerm = searchTerm.replace("!" + availableClass, "");
-		} else if (searchTerm.includes(availableClass)) {
-			allowedClasses.push(availableClass);
-			searchTerm = searchTerm.replace(availableClass, "");
-		}
-	}
+  let searchTerm = document.querySelector(".search").value;
 
-	const searchRegex = getSearchRegex(searchTerm);
-	searchTerm = searchTerm.toLowerCase();
+  const allowedClasses = [];
+  const filteredClasses = [];
+  const availableClasses = ["unlisted", "removed"];
+  for (const availableClass of availableClasses) {
+    if (searchTerm.includes("!" + availableClass)) {
+      filteredClasses.push(availableClass);
+      searchTerm = searchTerm.replace("!" + availableClass, "");
+    } else if (searchTerm.includes(availableClass)) {
+      allowedClasses.push(availableClass);
+      searchTerm = searchTerm.replace(availableClass, "");
+    }
+  }
 
-	document.querySelectorAll('.searchable').forEach((e) => {
-		let filtered = false;
-		for (const c of allowedClasses) {
-			if (!e.querySelector(`.${c}`)) filtered = true;
-		}
-		for (const c of filteredClasses) {
-			if (e.querySelector(`.${c}`)) filtered = true;
-		}
+  const searchRegex = getSearchRegex(searchTerm);
+  searchTerm = searchTerm.toLowerCase();
 
-    	if (!filtered && (searchTerm === "" ||
-    		(!searchRegex && e.dataset.search.toLowerCase().includes(searchTerm)) ||
-    		(searchRegex && searchRegex.test(e.dataset.search))
-    	)) {
-    		e.classList.remove("hide");
-    	} else {
-    		e.classList.add("hide");
-    	}
-	});
+  document.querySelectorAll('.searchable').forEach((e) => {
+    let filtered = false;
+    for (const c of allowedClasses) {
+      if (!e.querySelector(`.${c}`)) filtered = true;
+    }
+    for (const c of filteredClasses) {
+      if (e.querySelector(`.${c}`)) filtered = true;
+    }
+
+    if (!filtered && (searchTerm === "" ||
+        (!searchRegex && e.dataset.search.toLowerCase().includes(searchTerm)) ||
+        (searchRegex && searchRegex.test(e.dataset.search))
+      )) {
+      e.classList.remove("hide");
+    } else {
+      e.classList.add("hide");
+    }
+  });
 }
 
 window.addEventListener("load", () => {
-	channelSearch();
+  channelSearch();
 });
